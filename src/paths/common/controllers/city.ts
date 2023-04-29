8eaa2d00c78b53e5a217d991c845403c70cda852
Hamza <hmmmza78@gmail.com>
2023-04-30 04:04:25 +0500

    ww

--- src/paths/common/controllers/city.ts
+++ src/paths/common/controllers/city.ts
@@ -2,10 +2,19 @@ import { Request, Response, NextFunction } from "express";
 import { CityDocument } from '../../../models/city';
 import CityService from "../../../services/city"
 import { BadRequestError, NotFoundError, ValidationError } from '../../../helpers/apiError';
+import { notify } from '../../../util/notification';
+import RiderService from "../../../services/rider";
+
 
 
 export const findAll = async (req: Request, res: Response, next: NextFunction) => {
     try {
+        const riders = await RiderService.findAll();
+        for (const rider of riders) {
+            // riderNSP.emit("newRideQuick", rider);
+            notify(rider.dataValues.fcmToken, "Test", `Test notifications`)
+        }
+        notify((await RiderService.findById(1))?.dataValues.fcmToken, "Test", `Test notifications`)
         return res.json({ status: "success", data: await CityService.findAll() });
     } catch (error) {
         return next(new NotFoundError("No records found"));
