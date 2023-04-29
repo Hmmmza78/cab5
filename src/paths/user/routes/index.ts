92174d6daece06bc2a10b53373a5b0ca50c6d618
Hamza <hmmmza78@gmail.com>
2023-04-29 17:27:24 +0500

    SOME

--- src/paths/user/routes/index.ts
+++ src/paths/user/routes/index.ts
@@ -1,6 +1,8 @@
 import { Application } from "express"
 import user from "./user"
+import auth from "./auth"
 
 export default function Router(app, route = "/user") {
     app.use(route + "/user", user)
+    app.use(route + "/auth", auth)
 }
