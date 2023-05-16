852a835388cd19246671b29b321aa7740bd1df73
Hamza <hmmmza78@gmail.com>
2023-05-16 17:55:08 +0500

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Application } from "express";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 import ROUTER from "./router";
 ROUTER(app);
